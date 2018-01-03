d3d04146a8436d7214a65e43eecd7a579ebd73e8
Jan Rudof <jan.rudof@gmail.com>
2018-01-03 19:39:35 +0100

    Dodane tabele - problem šumniki

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -11,7 +11,7 @@ uvozi.tabela1 <- function(){
   tabela1 <- tabela1[c(3,1,2,4)]
   tabela1 <- tabela1 %>% fill(2:3)
   tabela1$MERITEV <- tabela1$MERITEV %>% parse_number()
-  tabela1 <- tabela1 %>% fill(2:3) %>% drop_na(1)
+  tabela1 <- tabela1 %>% fill(1:4) %>% drop_na(1)
   tabela1$MERITEV <- parse_integer(tabela1$MERITEV)
   tabela1 <- tabela1 %>% arrange(CETRTLETJE)
   tabela1 <- tabela1 %>% mutate(LETO = CETRTLETJE %>% strapplyc("^([0-9]+)") %>%
@@ -35,7 +35,7 @@ uvozi.tabela2 <- function(){
   tabela2 <- tabela2[c(3,1,2,4)]
   tabela2 <- tabela2 %>% fill(2:3)
   tabela2$MERITEV <- tabela2$MERITEV %>% parse_number()
-  tabela2 <- tabela2 %>% fill(2:3) %>% drop_na(1)
+  tabela2 <- tabela2 %>% fill(2:4) %>% drop_na(1)
   tabela2$MERITEV <- parse_integer(tabela2$MERITEV)
   tabela2 <- tabela2 %>% arrange(CETRTLETJE)
   tabela2 <- tabela2 %>% mutate(LETO = CETRTLETJE %>% strapplyc("^([0-9]+)") %>%
@@ -46,87 +46,73 @@ uvozi.tabela2 <- function(){
   return(tabela2)
 }
 tabela2 <- uvozi.tabela2()
+View(tabela2)
 
-
-#Tabela 5: Zasebna potovanja po destinaciji in nastanitvenem objektu
-uvozi.tabela5 <- function(){
-  stolpci5 <- c("DESTINACIJA", "VRSTA_NASTANITVE", "VRSTA_MERITVE", "CETRTLETJE", "MERITEV")
-  tabela5 <- read_csv2("podatki/NASTANITEV.csv",
+#Tabela 3: Zasebna potovanja po destinaciji in nastanitvenem objektu
+uvozi.tabela3 <- function(){
+  stolpci3 <- c("DESTINACIJA", "VRSTA_NASTANITVE", "VRSTA_MERITVE", "CETRTLETJE", "MERITEV")
+  tabela3 <- read_csv2("podatki/NASTANITEV.csv",
                        locale = locale(encoding = "cp1250"),
                        col_names = stolpci5,
                        skip = 2,
                        n_max = 1094,
                        na=c(""," ", "-","N"))
-  tabela5 <- tabela5 %>% fill(1:3)
-  tabela5$MERITEV <- tabela5$MERITEV %>% parse_number()
-  tabela5 <- tabela5[c(4,1,2,3,5)]
-  tabela5 <- tabela5 %>% fill(2:3) %>% drop_na(1)
-  tabela5 <- tabela5 %>% arrange(CETRTLETJE)
-  tabela5 <- tabela5 %>% mutate(LETO = CETRTLETJE %>% strapplyc("^([0-9]+)") %>%
+  tabela3 <- tabela3 %>% fill(1:3)
+  tabela3$MERITEV <- tabela3$MERITEV %>% parse_number()
+  tabela3 <- tabela3[c(4,1,2,3,5)]
+  tabela3 <- tabela3 %>% fill(1:5) %>% drop_na(1)
+  tabela3 <- tabela3 %>% arrange(CETRTLETJE)
+  tabela3 <- tabela3 %>% mutate(LETO = CETRTLETJE %>% strapplyc("^([0-9]+)") %>%
                                   unlist() %>% parse_number(),
                                 CETRTLETJE = CETRTLETJE %>% strapplyc("([0-9])$") %>%
                                   unlist() %>% parse_number())
-  tabela5 <- tabela5[c(6,1,2,3,4,5)]
+  tabela3 <- tabela3[c(6,1,2,3,4,5)]
+  return(tabela3)
+}
+tabela3 <- uvozi.tabela3()
+
+#Tabela 4: Zasebna potovanja po destinaciji in prevoznem sredstvu
+uvozi.tabela4 <- function(){
+  stolpci4 <- c("DESTINACIJA", "VRSTA_PREVOZA", "VRSTA_MERITVE", "CETRTLETJE", "MERITEV")
+  tabela4 <- read_csv2("podatki/PREVOZNO_SREDSTVO.csv",
+                       locale = locale(encoding= "cp1250"),
+                       col_names = stolpci4, 
+                       skip = 2,
+                       n_max = 1452,
+                       na = c("", " ", "-", "N"))
+  tabela4 <- tabela4 %>% fill(1:3)
+  tabela4 <- tabela4 %>% drop_na(1:5)
+  tabela4 <- tabela4 %>% arrange(CETRTLETJE)
+  tabela4 <- tabela4 %>% mutate(LETO = CETRTLETJE %>% strapplyc("^([0-9]+)") %>%
+                                  unlist() %>% parse_number(),
+                                CETRTLETJE = CETRTLETJE %>% strapplyc("([0-9])$") %>%
+                                  unlist() %>% parse_number())
+  tabela4$MERITEV <- tabela4$MERITEV %>% parse_number()
+  tabela4 <- tabela4[c(6,4,1,2,3,5)]
+  return(tabela4)
+}
+tabela4 <- uvozi.tabela4()
+
+#Tabela 5: Najbolj obiskane evrpske države in celine
+uvozi.tabela5 <- function(){
+  stolpci5 <- c("VRSTA_POTOVANJA", "DRZAVA_POTOVANJA", "VRSTA_MERITVE", "LETO", "MERITVE")
+  tabela5 <- read_csv2("podatki/DRZAVA_POTOVANJA.csv",
+                       locale = locale(encoding= "cp1250"),
+                       col_names= stolpci5,
+                       skip = 2,
+                       n_max = 1054,
+                       na = c("", " ", "-", "N"))
+  tabela5 <- tabela5 %>% fill(1:4)
+  tabela5 <- tabela5 %>% arrange(LETO)
+  tabela5 <- tabela5 %>% drop_na(1:5)
+  tabela5$MERITVE <- tabela5$MERITVE %>% parse_number()
+  tabela5 <- tabela5[c(4,1,2,3,5)]
   return(tabela5)
 }
 tabela5 <- uvozi.tabela5()
+View(tabela5)
 
 
 
 
 
-
-#Pomoč s primerom:
-
-sl <- locale("sl", decimal_mark = ",", grouping_mark = ".")
-
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec = ",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na = "-", locale = sl)
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
-
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names = c("obcina", 1:4),
-                    locale = locale(encoding = "Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse = " ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars = "obcina", variable.name = "velikost.druzine",
-                        value.name = "stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels = obcine)
-  return(data)
-}
-
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
-
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
-
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
